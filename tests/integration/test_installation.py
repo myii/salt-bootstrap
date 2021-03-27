d2c6bb33c456dc6492bfa179a7c2cfd0cfc2213e
Imran Iqbal <iqbalmy@hotmail.com>
2021-03-27 14:17:10 +0000

    test: use short timeout to see output

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -9,18 +9,18 @@ log = logging.getLogger(__name__)
 
 def test_ping(host):
     with host.sudo():
-        assert host.salt("test.ping", "--timeout=120")
+        assert host.salt("test.ping", "--timeout=5")
 
 
 def test_target_python_version(host, target_python_version):
     with host.sudo():
-        ret = host.salt("grains.item", "pythonversion", "--timeout=120")
+        ret = host.salt("grains.item", "pythonversion", "--timeout=5")
         assert ret["pythonversion"][0] == target_python_version
 
 
 def test_target_salt_version(host, target_salt_version):
     with host.sudo():
-        ret = host.salt("grains.item", "saltversion", "--timeout=120")
+        ret = host.salt("grains.item", "saltversion", "--timeout=5")
         if target_salt_version.endswith(".0"):
             assert ret["saltversion"] == ".".join(target_salt_version.split(".")[:-1])
         else:
