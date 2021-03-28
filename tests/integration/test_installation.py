4f8dd8b6e8abe1961563a8304f84daed768d3934
Imran Iqbal <iqbalmy@hotmail.com>
2021-03-28 04:18:10 +0100

    test: avoid `sudo` while testing on Windows

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -3,23 +3,34 @@ import json
 import pytest
 import logging
 import pprint
+from contextlib import nullcontext
 
 log = logging.getLogger(__name__)
 
+# if needs_with():
+#     cm = get_stuff()
+# else:
+#     cm = nullcontext()
+#
+# with cm as gs:
+#     # Do stuff
 
 def test_ping(host):
-    with host.sudo():
+    # with host.sudo():
+    with nullcontext():
         assert host.salt("test.ping", "--timeout=5")
 
 
 def test_target_python_version(host, target_python_version):
-    with host.sudo():
+    # with host.sudo():
+    with nullcontext():
         ret = host.salt("grains.item", "pythonversion", "--timeout=5")
         assert ret["pythonversion"][0] == target_python_version
 
 
 def test_target_salt_version(host, target_salt_version):
-    with host.sudo():
+    # with host.sudo():
+    with nullcontext():
         ret = host.salt("grains.item", "saltversion", "--timeout=5")
         if target_salt_version.endswith(".0"):
             assert ret["saltversion"] == ".".join(target_salt_version.split(".")[:-1])
