5b420118cbbc4be66072b70428e77496fc596299
Imran Iqbal <iqbalmy@hotmail.com>
2021-03-28 04:36:17 +0100

    test: fixup

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -3,7 +3,7 @@ import json
 import pytest
 import logging
 import pprint
-from contextlib import nullcontext
+# from contextlib import nullcontext
 
 log = logging.getLogger(__name__)
 
@@ -17,22 +17,22 @@ log = logging.getLogger(__name__)
 
 def test_ping(host):
     # with host.sudo():
-    with nullcontext():
-        assert host.salt("test.ping", "--timeout=5")
+    # with nullcontext():
+    assert host.salt("test.ping", "--timeout=5")
 
 
 def test_target_python_version(host, target_python_version):
     # with host.sudo():
-    with nullcontext():
-        ret = host.salt("grains.item", "pythonversion", "--timeout=5")
-        assert ret["pythonversion"][0] == target_python_version
+    # with nullcontext():
+    ret = host.salt("grains.item", "pythonversion", "--timeout=5")
+    assert ret["pythonversion"][0] == target_python_version
 
 
 def test_target_salt_version(host, target_salt_version):
     # with host.sudo():
-    with nullcontext():
-        ret = host.salt("grains.item", "saltversion", "--timeout=5")
-        if target_salt_version.endswith(".0"):
-            assert ret["saltversion"] == ".".join(target_salt_version.split(".")[:-1])
-        else:
-            assert ret["saltversion"].startswith(target_salt_version)
+    # with nullcontext():
+    ret = host.salt("grains.item", "saltversion", "--timeout=5")
+    if target_salt_version.endswith(".0"):
+        assert ret["saltversion"] == ".".join(target_salt_version.split(".")[:-1])
+    else:
+        assert ret["saltversion"].startswith(target_salt_version)
