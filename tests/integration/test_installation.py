e6afc2b6edc74264a95efd6285e1ff8a3f40af34
Imran Iqbal <iqbalmy@hotmail.com>
2021-03-28 09:36:49 +0100

    fix(test_installation): use function for selecting context manager

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -8,24 +8,26 @@ from contextlib import nullcontext
 
 log = logging.getLogger(__name__)
 
-if "windows" in os.environ.get("KITCHEN_INSTANCE"):
-    cm = nullcontext()
-else:
-    cm = host.sudo()
+
+def selected_context_manager(host):
+    if "windows" in os.environ.get("KITCHEN_INSTANCE"):
+        return nullcontext()
+    return host.sudo()
+
 
 def test_ping(host):
-    with cm:
+    with selected_context_manager(host):
         assert host.salt("test.ping", "--timeout=120")
 
 
 def test_target_python_version(host, target_python_version):
-    with cm:
+    with selected_context_manager(host):
         ret = host.salt("grains.item", "pythonversion", "--timeout=120")
         assert ret["pythonversion"][0] == target_python_version
 
 
 def test_target_salt_version(host, target_salt_version):
-    with cm:
+    with selected_context_manager(host):
         ret = host.salt("grains.item", "saltversion", "--timeout=120")
         if target_salt_version.endswith(".0"):
             assert ret["saltversion"] == ".".join(target_salt_version.split(".")[:-1])
